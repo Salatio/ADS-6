240383edf8a6923f31c0a6aea7cae30ca74dcf33
Salatio <99189453+Salatio@users.noreply.github.com>
2022-04-13 20:45:12 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,8 +1,7 @@
 // Copyright 2022 NNTU-CS
-#include <string>
-
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
@@ -10,6 +9,7 @@ class TPQueue {
     T arr[100];
     int first;
     int last;
+
  public:
     TPQueue(): first(0), last(0) { }
     void push(T x) {
