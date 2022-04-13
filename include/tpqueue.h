cc2b3dca5c992de38298757f9becd3b96c7b2fff
Salatio <99189453+Salatio@users.noreply.github.com>
2022-04-13 20:19:34 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,6 +5,45 @@
 template<typename T, int size>
 class TPQueue {
   // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T arr[size];
+    int first;
+    int last;
+ public:
+    TPQueue(): first(0), last(0) { }
+    void push(T x) {
+        if (last - first >= size) {
+            throw std::string("Full!");
+        } else {
+              if (!getSize()) {
+                  arr[(last++) % size] = x;
+              } else {
+                    for (int i = first; i < last; ++i) {
+                        if (arr[i].prior < x.prior) {
+                            for (int j = last; j > i; --i) {
+                                arr[j % size] = arr[(j % size) - 1];
+                            }
+                            last++;
+                            arr[i] = x;
+                            return;
+                        }
+                    }
+                    arr[(last++) % size] = x;
+              }
+        }
+    }
+    T pop() {
+        return arr[(first++) % size];
+    }
+    int getSize() {
+        return (last - first);
+    }
+    T front() {
+        return arr[first % size];
+    }
+    T back() {
+        return arr[(last - 1) % size];
+    }
 };
 
 struct SYM {
